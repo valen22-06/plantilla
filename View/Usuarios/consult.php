4a0ecb2031a8b06119a0ae37c05489a1fbcd35bf
Juan <juanangelrespi20052@gmail.com>
2024-11-27 13:26:40 -0500

    xd6

--- View/Usuarios/consult.php
+++ View/Usuarios/consult.php
@@ -2,7 +2,7 @@
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script> 
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <?php
-include_once '../../Lib/helpers.php'; 
+include_once '../Lib/helpers.php'; 
 
 ?>
 <div class="mt 3">
