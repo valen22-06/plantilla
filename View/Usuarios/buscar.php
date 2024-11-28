061d64635c3980c1da6c56eeda643888aedfae17
Juan <juanangelrespi20052@gmail.com>
2024-11-27 21:49:49 -0500

    xd11

---
+++ View/Usuarios/buscar.php
@@ -0,0 +1,47 @@
+<?php
+                foreach($usuarios as $usu){
+                    $clase="";
+                    $texto="";
+                    echo "<tr>";
+                        echo "<td>".$usu['id_usuario']."</td>";
+                        echo "<td>".$usu['id_tipo_documento']."</td>";
+                        echo "<td>".$usu['numero_documento']."</td>";
+                        echo "<td>".$usu['primer_nombre'].$usu['segundo_nombre']."</td>";
+                        // echo "<td>".$usu['segundo_nombre']."</td>";
+                        echo "<td>".$usu['primer_apellido'].$usu['segundo_apellido']."</td>";
+                        // echo "<td>".$usu['segundo_apellido']."</td>";
+                        echo "<td>".$usu['telefono']."</td>";
+                        echo "<td>".$usu['correo']."</td>";
+                        echo "<td>".$usu['direcion_residencia']."</td>";
+                        echo "<td>".$usu['fecha_nacimiento']."</td>";
+
+                        if($usu['id_estado']==1){
+                            $clase="btn btn-danger";
+                            $texto="Inhabilitar";
+                        }else if($usu['id_estado']==2){
+                            $clase="btn btn-success";
+                            $texto="Habilitar";
+                        }
+                         
+                        echo "<td>";
+                            if(!empty($clase))echo "<button type='button' class='$clase' id='cambiar_estado' 
+                            data-url='" .getUrl("Usuarios", "Usuarios", "postUpdateStatus", false, "ajax"). "' 
+                            data-id='" .$usu['id_estado'] ."' 
+                            data-user='" .$usu['id_usuario'] ."'>$texto</button>"
+                            ."</td>";
+                        
+                            echo "<td>";
+                            echo "<a href='" . getUrl("Usuarios", "Usuarios", "getUpdate", array("id_usuario" => $usu['id_usuario'])) . "'>";
+                            echo "<button class='btn btn-primary'>Editar</button>";
+                            echo "</a>";
+                            echo "</td>";
+
+                            echo "<td>";
+                            echo "<a href='" . getUrl("Usuarios", "Usuarios", "getDelete", array("id_usuario" => $usu['id_usuario'])) . "'>";
+                            echo "<button class='btn btn-danger'>Eliminar</button>";
+                            echo "</a>";
+                            echo "</td>";
+                        
+                    echo "</tr>";
+                }
+            ?>
